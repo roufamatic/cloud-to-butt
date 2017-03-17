ffc77274fa8b464f9fef98b7140e0f44dc85fd3e
Michael Roufa <roufamatic@gmail.com>
2017-03-16 20:07:08 -0700

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,10 +36,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\b(False|Falsehoods?)\b/g, "Bullshit);
+	v = v.replace(/\b(false|falsehoods?)\b/g, "bullshit");
 	
 	textNode.nodeValue = v;
 }
