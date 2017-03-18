0812ffe509fe58a75f275815cc6c8b222fdffd5b
Michael Roufa <mike@roufa.com>
2017-03-18 13:40:03 -0700

    it works

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,45 +1,43 @@
-walk(document.body);
-
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
+(function(justHeaders) {
+	if (justHeaders) {
+		["h1","h2","h3","h4","h5","h6"].forEach((tag) => {
+			flatten(document.getElementsByTagName(tag)).forEach((el) => {
+				textNodesUnder(el).forEach((node) => {
+					handleText(node);
+				});
+			});
+		});
 	}
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
+	else {
+		textNodesUnder(document.body).forEach((node) => {
 			handleText(node);
-			break;
+		});
 	}
+})(false);
+
+function textNodesUnder(el){
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
 }
 
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\b(False|Falsehoods?)\b/g, "Bullshit");
-	v = v.replace(/\b(false|falsehoods?)\b/g, "bullshit");
-	
+	v = v.replace(/\bA\s([^\s+])\sFalsehood\b/g, "A Piece of $1 Bullshit");
+	v = v.replace(/\bA\s([^\s+])\sfalsehood\b/g, "A piece of $1 bullshit");
+	v = v.replace(/\ba\s([^\s+])\sFalsehood\b/g, "a piece of $1 Bullshit");
+	v = v.replace(/\ba\s([^\s+])\sfalsehood\b/g, "a piece of $1 bullshit");
+	v = v.replace(/Falsehoods?/g, "Bullshit");
+	v = v.replace(/falsehoods?/g, "bullshit");
+	v = v.replace(/\b(False)\b/g, "Full of Shit");
+	v = v.replace(/\b(false)\b/g, "full of shit");
+
 	textNode.nodeValue = v;
 }
 
+function flatten(list) {
+	return (Array.from(list)).reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+}
 
